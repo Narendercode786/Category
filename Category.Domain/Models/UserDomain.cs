f2bdb2c276d50312097f98260b78980bb4a34a81
Narender <kumar786narender@gmail.com>
2023-09-17 16:32:38 +0530

    changes

--- Category.Domain/Models/UserDomain.cs
+++ Category.Domain/Models/UserDomain.cs
@@ -5,8 +5,10 @@ namespace Category.Domain.Models
     public class UserDomain
     {
         [Required]
+        [Display(Name = "User Id")]
         public string UserName { get; set; }
         [Required]
+        [Display(Name = "Password")]
         public string Password { get; set; }
     }
 }
